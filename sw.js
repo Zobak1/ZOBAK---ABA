5cd40ab07e902a2aa7dcf2eff5f4412a85d39e56
Zobak1 <zobak1@centrum.cz>
2026-08-08 11:11:29 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE='abu-pwa-v1-0-0-preloaded-tickets';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png',
+  './favicon-32.png',
+  './aba-sidebar-logo.png',
+  './aba-watermark.png',
+  './kodex-spravce-kapitalu.png'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('message',event=>{
+  if(event.data && event.data.type==='SKIP_WAITING')self.skipWaiting();
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+
+  if(url.pathname.endsWith('/') || url.pathname.endsWith('/index.html')){
+    event.respondWith(
+      fetch(event.request,{cache:'no-store'})
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+          return response;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached=>
+      cached || fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        return response;
+      })
+    )
+  );
+});
